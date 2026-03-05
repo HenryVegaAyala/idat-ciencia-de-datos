dc966b3ab7e587ccd40bb62f15d3eacf89b9154e
Henry Vega <hp.vega@hotmail.com>
2026-03-04 21:10:37 -0500

    Update marketing data and enhance prediction logic in marketing.py

--- sesion-7/marketing.py
+++ sesion-7/marketing.py
@@ -5,20 +5,33 @@ from sklearn.linear_model import LinearRegression
 # 2. Leer archivo CSV
 data = pd.read_csv('marketing.csv')
 
-x = data[['marketing']] # ahora, para evitar el error de forma de entrada, se selecciona la columna 'marketing' como un DataFrame en lugar de una Serie
+# ahora, para evitar el error de forma de entrada, se selecciona la columna Ej 'marketing' como un DataFrame en lugar de una Serie
+x = data[['marketing']]
+print(f"-"*60)
 print(f"{x}")
+print(f"-"*60)
 
+# ahora se selecciona la columna 'ventas' como una Serie
 y = data['ventas']
+print(f"*"*60)
 print(f"{y}")
+print(f"*"*60)
 
 # 3. Creamos y entrenamos el modelo de regresión lineal
 modelo = LinearRegression()
-modelo.fit(x, y)
+modelo.fit(x, y) # se entrena el modelo con los datos de marketing (x) y ventas (y)
 
 # 4. Hacer predicciones
-# prediccion = modelo.predict([[6]]) # antes
-column = pd.DataFrame([[9]], columns=['marketing'])
+
+# Crearmos un nuevo registro para la predicción.
+column = pd.DataFrame([[6000]], columns=['marketing'])
+print(f"Columna de entrada para predicción:\n{column}")
 prediccion = modelo.predict(column) # ahora, para evitar el error de forma de entrada, se crea un DataFrame con la misma estructura que el conjunto de datos original
 
 # 5. Imprimir el resultado
-print(f"La predicción de ventas para una inversión de $9000 en marketing es: {prediccion[0]*1000}")
+print(f"La predicción de ventas para una inversión de $6000 en marketing es: {prediccion[0]}")
+
+# Otra predicción con un nuevo valor
+column = pd.DataFrame([[10000]], columns=['marketing'])
+prediccion = modelo.predict(column)
+print(f"La predicción de ventas para una inversión de $10000 en marketing es: {prediccion[0]}")
